a05bc43d52b7d152093347b1e2eaffa4888cb835
donald <onsemise@qq.com>
2019-08-05 11:25:47 +0800

    androidx

--- adapterx/src/main/java/com/dhy/adapterx/Adapter.kt
+++ adapterx/src/main/java/com/dhy/adapterx/Adapter.kt
@@ -5,8 +5,6 @@ import android.view.View
 import androidx.annotation.LayoutRes
 import androidx.recyclerview.widget.RecyclerView
 import kotlin.reflect.KClass
-import kotlin.reflect.full.primaryConstructor
-import kotlin.reflect.jvm.isAccessible
 
 open class AdapterX<HOLDER : IViewHolder<DATA>, DATA>(
     context: Context,
@@ -51,15 +49,16 @@ fun <HOLDER : RecyclerView.ViewHolder> getLayoutId(holder: KClass<HOLDER>): Int 
     return holder.java.getAnnotation(LayoutId::class.java)!!.value
 }
 
+@Suppress("UNCHECKED_CAST")
 fun <HOLDER : RecyclerView.ViewHolder> getHolderCreator(holder: KClass<HOLDER>, vararg args: Any?): ((View) -> HOLDER) {
-    val constructor = holder.primaryConstructor!!
+    val constructor = holder.java.constructors.first()
     if (!constructor.isAccessible) constructor.isAccessible = true
-    if (args.isEmpty()) return { constructor.call(it) }
+    if (args.isEmpty()) return { constructor.newInstance(it) as HOLDER }
     else {
         val params = args.toMutableList().apply { add(0, null) }.toTypedArray()
         return {
             params[0] = it
-            constructor.call(*params)
+            constructor.newInstance(*params) as HOLDER
         }
     }
 }
